18a381419559ae307797f6e050697419e0d448c3
sigge <siggelabor@hotmail.com>
2022-10-29 00:24:47 +0200

    Adding radix 10 to parseInt

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ export const get = (key: number, length = 6): Sigkey => {
 };
 
 export const inc = (key: string, increment = 1): Sigkey => {
-    return get(parseInt(key) + increment, key.length);
+    return get(parseInt(key, 10) + increment, key.length);
 };
 
 export const dec = (key: string, decrement = 1): Sigkey => {
-    return get(parseInt(key) - decrement, key.length);
+    return get(parseInt(key, 10) - decrement, key.length);
 };
 
 export const parse = (key: string): number => {
-    return parseInt(key);
+    return parseInt(key, 10);
 };
